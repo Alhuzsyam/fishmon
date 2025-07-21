f3f124fd5d9b70da35e8c5d7128a884ba946d43c
Alfi Alhuzwiri Syam <alhuzwirialfi@gmail.com>
2025-07-21 19:15:48 +0700

    update port

--- src/main/java/com/fishmon/Config/SecurityConfig.java
+++ src/main/java/com/fishmon/Config/SecurityConfig.java
@@ -96,8 +96,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
             "http://localhost:5500",
             "http://127.0.0.1:5500",
-            "http://localhost:5174",
-            "http://127.0.0.1:5174"
+            "http://localhost:5173",
+            "http://127.0.0.1:5173"
             ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
