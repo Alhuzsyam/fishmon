98d6640f430366b592804d236b727324af9fc484
Alfi Alhuzwiri Syam <alhuzwirialfi@gmail.com>
2025-07-28 23:23:33 +0700

    change cros port

--- src/main/java/com/fishmon/Config/SecurityConfig.java
+++ src/main/java/com/fishmon/Config/SecurityConfig.java
@@ -96,8 +96,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-            "http://localhost:5500",
-            "http://127.0.0.1:5500",
+            "http://localhost:80",
+            "http://127.0.0.1:80",
             "http://localhost:5173",
             "http://127.0.0.1:5173"
             ));
